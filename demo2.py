eceaaa1ae9dbd0068b6caea323390903f17297f7
LYH2846579 <1557902391@qq.com>
2021-07-17 16:18:38 +0800

    Python基础学习

---
+++ demo2.py
@@ -0,0 +1,10 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2021/7/17 14:59
+# @Auther : 亱華
+# @File : demo2.py
+# @SoftWare : PyCharm
+
+import bs4
+import re
+import urllib.request,urllib.error
+import xlwt
