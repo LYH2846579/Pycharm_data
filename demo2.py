476ac3f4a8b0a16a9d92891a91117219029169ba
LYH2846579 <1557902391@qq.com>
2021-07-17 17:24:05 +0800

    Python基础学习

--- demo2.py
+++ demo2.py
@@ -8,3 +8,53 @@ import bs4
 import re
 import urllib.request,urllib.error
 import xlwt
+
+def main():
+    baseurl = "https://movie.douban.com/top250?start="
+    #1.爬取网页
+    datalist = getData(baseurl)
+    #2.逐一解析数据       ->    在爬取过程中边爬取边处理
+
+
+    #3.保存数据
+    savepath = ".\\豆瓣电影Top250.xls"
+    #saveData(savepath)
+    askURL("https://movie.douban.com/top250")
+
+#得到一个指定URL的网页内容
+def askURL(url):
+    #伪装成为浏览器访问网站
+    head = {    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36","X-Amzn-Trace-Id": "Root=1-60f297ad-062adad97fe836221ed8040c"
+        }
+    request = urllib.request.Request(url,headers=head)
+    html = ""
+    try:
+        response = urllib.request.urlopen(request)
+        html = response.read().decode("utf-8")
+        #print(html)
+    except urllib.error.URLError as e:
+        if hasattr(e,"code"):
+            print(e.code)
+        if hasattr(e,"reason"):
+            print(e.reason)
+    return html
+
+
+
+#爬取网页
+def getData(baseurl):
+    datalist = []
+    for i in range(0,10):                   #爬取十次页面
+        url = baseurl+str(i*25)
+        html = askURL(url)                  #保存获取的网页原码
+    return datalist
+
+#保存数据
+def saveData(savepath):
+    print("数据已保存")
+
+
+
+if __name__ == "__main__":          #当程序执行时
+#调用函数
+    main()
---
+++ venv/urllib/test1.py
@@ -0,0 +1,43 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2021/7/17 16:31
+# @Auther : 亱華
+# @File : test1.py
+# @SoftWare : PyCharm
+
+import urllib.request
+# reponse = urllib.request.urlopen("http://www.baidu.com")
+# print(reponse.read().decode('utf-8'))
+# f = open("baidu.txt","w")
+# f.write(reponse.read().decode('utf-8'))
+# f.close()
+
+#获取一个post请求
+import urllib.parse
+# data = bytes(urllib.parse.urlencode({"hello":"world"}),encoding="utf-8")
+# response = urllib.request.urlopen("http://httpbin.org/post",data=data)
+# print(response.read().decode("utf-8"))
+
+#伪装
+# url = "http://douban.com"
+# headers = {
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
+#     "X-Amzn-Trace-Id": "Root=1-60f297ad-062adad97fe836221ed8040c"
+# }
+# data = bytes(urllib.parse.urlencode({"hello":"world"}),encoding="utf-8")
+# req = urllib.request.Request(url=url,data=data,headers=headers,method="POST")
+# response = urllib.request.urlopen(req)
+# print(response.read().decode("utf-8"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
